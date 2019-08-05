ef69d076fc10cd863e5ecb19188ad18144965b72
MatthewDodi <matthew.dodi@gmail.com>
2019-08-04 19:22:05 -0700

    Added mutations

--- python-graphene/schema.py
+++ python-graphene/schema.py
@@ -1,23 +1,63 @@
 import graphene
 import json
+import uuid
+from datetime import datetime
+
+
+class User(graphene.ObjectType):
+    id = graphene.ID(default_value=str(uuid.uuid4()))
+    username = graphene.String()
+    created_at = graphene.DateTime(default_value=datetime.now())
 
 
 class Query(graphene.ObjectType):
+    users = graphene.List(User, limit=graphene.Int())
     hello = graphene.String()
+    is_admin = graphene.Boolean()
 
     def resolve_hello(self, info):
         return "world"
 
+    def resolve_is_admin(self, info):
+        return True
 
-schema = graphene.Schema(query=Query)
+    def resolve_users(self, info, limit=None):
+        return [
+            User(id="1", username="Matthew", created_at=datetime.now()),
+            User(id="2", username="Linda", created_at=datetime.now())
+        ][:limit]
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(User)
+
+    class Arguments:
+        username = graphene.String()
+
+    def mutate(self, info, username):
+        user = User(username=username)
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 result = schema.execute(
     '''
-    {
-        hello
+    mutation {
+        createUser(username: "Jack") {
+            user {
+                id
+                username
+                createdAt
+            }
+        }
     }
     '''
 )
 
 response = dict(result.data.items())
-print(json.dumps(response))
+print(json.dumps(response, indent=2))
